01f31dbcf8096244d6ca964ffd568b97dbdfb5c9
leductindeveloper <tinldfct31132@gmail.com>
2026-03-13 08:25:36 +0700

    Update upload.js

--- website/server/middleware/upload.js
+++ website/server/middleware/upload.js
@@ -1,21 +1,21 @@
-const multer = require("multer")
-const { CloudinaryStorage } = require("multer-storage-cloudinary")
-const cloudinary = require("cloudinary").v2
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinary.config({
- cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
- api_key: process.env.CLOUDINARY_API_KEY,
- api_secret: process.env.CLOUDINARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const storage = new CloudinaryStorage({
- cloudinary: cloudinary,
- params: {
-  folder: "classrecap",
-  allowed_formats: ["jpg", "png", "jpeg", "webp"]
- }
-})
+  cloudinary: cloudinary,
+  params: {
+    folder: "class-recap",
+    allowed_formats: ["jpg", "png", "jpeg"]
+  }
+});
 
-const upload = multer({ storage })
+const upload = multer({ storage });
 
-module.exports = upload
+module.exports = upload;
