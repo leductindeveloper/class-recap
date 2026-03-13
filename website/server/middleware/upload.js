6737e7c867d005eb00c4b28e710da536223d8b23
leductindeveloper <tinldfct31132@gmail.com>
2026-03-13 08:10:22 +0700

    Switch to Cloudinary for file uploads

--- website/server/middleware/upload.js
+++ website/server/middleware/upload.js
@@ -1,19 +1,21 @@
 const multer = require("multer")
-const path = require("path")
+const { CloudinaryStorage } = require("multer-storage-cloudinary")
+const cloudinary = require("cloudinary").v2
 
-const storage = multer.diskStorage({
-
-  destination: function (req, file, cb) {
-    cb(null, "public/uploads/")
-  },
-
-  filename: function (req, file, cb) {
-    const uniqueName = Date.now() + path.extname(file.originalname)
-    cb(null, uniqueName)
-  }
-
+cloudinary.config({
+ cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+ api_key: process.env.CLOUDINARY_API_KEY,
+ api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
-const upload = multer({ storage: storage })
+const storage = new CloudinaryStorage({
+ cloudinary: cloudinary,
+ params: {
+  folder: "classrecap",
+  allowed_formats: ["jpg", "png", "jpeg", "webp"]
+ }
+})
+
+const upload = multer({ storage })
 
 module.exports = upload
