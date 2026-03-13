9695325a45587c21655bf8988acffb9396b951f1
leductindeveloper <tinldfct31132@gmail.com>
2026-03-13 13:30:02 +0700

    Update adminController.js

--- website/server/controllers/adminController.js
+++ website/server/controllers/adminController.js
@@ -237,33 +237,6 @@ exports.deleteComment = async (req, res) => {
 
   res.redirect('/admin/comments');
 };
-// Admin Dashboard Area (Protected) - use admin layout
-router.use(ensureAuthenticated, (req, res, next) => {
-  res.locals.layout = 'admin/layout';
-  next();
-});
-
-router.get('/', adminController.getDashboard);
-
-// Recap Management
-router.get('/recaps', adminController.getRecaps);
-router.get('/recaps/create', adminController.getCreateRecap);
-router.post('/recaps/create', upload.single('coverImage'), adminController.postCreateRecap);
-router.get('/recaps/edit/:id', adminController.getEditRecap);
-router.post('/recaps/edit/:id', upload.single('coverImage'), adminController.postEditRecap);
-router.post('/recaps/delete/:id', adminController.deleteRecap);
-router.post('/recaps/toggle-featured/:id', adminController.toggleFeatured);
-
-// Gallery Management
-router.get('/gallery', adminController.getGallery);
-router.post('/gallery/create-album', adminController.postCreateAlbum);
-router.post('/gallery/upload/:albumId', upload.array('images', 10), adminController.postUploadImages);
-router.post('/gallery/delete-album/:id', adminController.deleteAlbum);
-router.post('/gallery/:albumId/image/delete/:imageId', adminController.deleteImage);
-
-// Comment Management
-router.get('/comments', adminController.getComments);
-router.post('/comments/approve/:id', adminController.approveComment);
 // DELETE ALBUM
 exports.deleteAlbum = async (req, res) => {
   try {
